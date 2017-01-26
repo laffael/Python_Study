7cad50765bd6472e0b2e7c145d82a8856c4e76c1
laffael <13914717717@163.com>
2017-01-26 15:41:58 +0800

    2017-01-26 push test

---
+++ practice.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Date    : 2016-11-03 15:20:52
+# @Author  : Your Name (you@example.org)
+# @Link    : http://example.org
+# @Version : $Id$
+
+
+print "hello"
